f31c10a2194d167521ab3dc5935e8723d54afa51
Mikael Carlsson <mikkar2@kth.se>
2020-02-03 16:23:05 +0100

    heyo

--- src/main/java/dd2480/group4/net/BuildRequest.java
+++ src/main/java/dd2480/group4/net/BuildRequest.java
@@ -23,8 +23,6 @@ public class BuildRequest {
         System.out.println("1.2");
         var mapper = new ObjectMapper();
         System.out.println("2.2");
-        System.out.println("\n\n\n");
-        System.out.println(json);
         var a = mapper.readValue(json, BuildRequest.class); //jidder here
         System.out.println("3.2");
         return a;
