8aca0487cd9349abfe7a13c2fc82ed662f696982
Mikael Carlsson <mikkar2@kth.se>
2020-02-03 16:29:13 +0100

    heyo

--- src/main/java/dd2480/group4/net/BuildRequest.java
+++ src/main/java/dd2480/group4/net/BuildRequest.java
@@ -2,6 +2,7 @@ package dd2480.group4.net;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.io.IOException;
 
@@ -9,6 +10,7 @@ import java.io.IOException;
  * Defines the structure of a request to build the project
  * TODO: should model the repository at a given state (i.e. url, commit hash, branch, time and date)
  */
+@JsonIgnoreProperties
 public class BuildRequest {
 
     @JsonProperty("git_url")
