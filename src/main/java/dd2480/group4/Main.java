38cd353767a0e4a7151af46ee1b9536f49c84a98
Mikael Carlsson <mikkar2@kth.se>
2020-02-03 14:35:49 +0100

    heyo

--- src/main/java/dd2480/group4/Main.java
+++ src/main/java/dd2480/group4/Main.java
@@ -7,9 +7,13 @@ public class Main {
     public static void main(String[] args) {
         Server server = new Server(8004);
         try {
+            System.out.println("1");
             server.setHandler(new RequestHandler(new Runner()));
+            System.out.println("2");
             server.start();
+            System.out.println("3");
             server.join();
+            System.out.println("4");
         } catch(Exception e) {
             System.err.println("dafuq");
         }
